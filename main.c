3c7fbf7075331e4284523f742381fe80d22083d8
AntoninSonka <antonin.sonka@outlook.com>
2024-01-08 13:48:19 +0100

    better readebility

--- main.c
+++ main.c
@@ -10,6 +10,25 @@ void printBinNum(int x){
     }
 }
 
+void printIpDec(int* byte, char* type){
+    //printf("%s: ", type);
+    for(int i = 0; i < 4; i++){
+        printf("%03d", byte[i]);
+        if(i != 3){
+            printf(".");
+        }
+    }
+}
+
+void printIpBin(int* byte, char* type){
+    for(int i = 0; i < 4; i++){
+        printBinNum(byte[i]);
+        if(i != 3){
+            printf(".");
+        }
+    }
+}
+
 void convertIpHelper(int* a, char* ip){
     char strA[5] = "";
     static int i = 0;
@@ -24,11 +43,9 @@ void convertIpHelper(int* a, char* ip){
 }
 
 void convertIp(int* byte, int* netmask, char* ip){
-    char strNetmask[2] = "";
-    convertIpHelper(&byte[0], ip);
-    convertIpHelper(&byte[1], ip);
-    convertIpHelper(&byte[2], ip);
-    convertIpHelper(&byte[3], ip);
+    for(int i = 0; i < 4; i++){
+        convertIpHelper(&byte[i], ip);
+    }
     convertIpHelper(netmask, ip);
 }
 
@@ -104,131 +121,83 @@ int main(int argc, char *argv[]){
 
     for(int i = 0; i < 4; i++){
         if(byte[i] < 0 || byte[i] > 255){
-            printf("worong value on adress on byte no.%d (indexed form 0)\n", i);
+            printf("wrong value on adress on byte no.%d (indexed form 0)\n", i);
             return 2;
         }
     }
 
     if(netmask < 1 || netmask > 32){
-        printf("worong value on netmask\n");
+        printf("wrong value on netmask\n");
         return 3;
     }
-    
-    printf("Adress: %d.%d.%d.%d ____ ", byte[0], byte[1], byte[2], byte[3]);
-    printf("Adress in bin: ");
-    //%08b.%08b.%08b.%08b\n", byte[0], byte[1], byte[2], byte[3]);
-    printBinNum(byte[0]);
-    printf(".");
-    printBinNum(byte[1]);
-    printf(".");
-    printBinNum(byte[2]);
-    printf(".");
-    printBinNum(byte[3]);
+
     if(byte[0] < 248){
-        printf("      Class %c\n", ((byte[0] < 128) ? 'A' : ((byte[0] < 192) ? 'B' : ((byte[0] < 224) ? 'C' : ((byte[0] < 240) ? 'D' : 'E')))));
+        printf("(Class %c)\n\n", ((byte[0] < 128) ? 'A' : ((byte[0] < 192) ? 'B' : ((byte[0] < 224) ? 'C' : ((byte[0] < 240) ? 'D' : 'E')))));
     }
     else{
-        printf("      Undefined Class\n");
+        printf("(Undefined Class)\n\n");
     }
+
+    printf("Adress:   ");
+    printIpDec(byte, "Adress");
+    printf("           ");
+    printIpBin(byte, "Adress");
+    printf("\n");
     
     int netmaskByte[4] = {255, 255, 255, 255};
     calculateNetmaskAndNetwork(netmaskByte, netmask);
-
-    printf("Netmask: %d.%d.%d.%d = %d ____ ", netmaskByte[0], netmaskByte[1], netmaskByte[2], netmaskByte[3], netmask);
-    printf("Netmask in bin: ");
-    //%08b.%08b.%08b.%08b\n", netmaskByte[0], netmaskByte[1], netmaskByte[2], netmaskByte[3]);
     
-    printBinNum(netmaskByte[0]);
-    printf(".");
-    printBinNum(netmaskByte[1]);
-    printf(".");
-    printBinNum(netmaskByte[2]);
-    printf(".");
-    printBinNum(netmaskByte[3]);
+    printf("Netmask:  ");
+    printIpDec(netmaskByte, "Netmask");
+    printf(" = %d      ", netmask);
+    printIpBin(netmaskByte, "Netmask");
     printf("\n");
 
     for(int i = 0; i < 4; i++){
         netmaskByte[i] = netmaskByte[i] ^ 0b11111111;
     }
-
-    printf("Wildcard: %d.%d.%d.%d ____ ", netmaskByte[0], netmaskByte[1], netmaskByte[2], netmaskByte[3]);
-    printf("Wildcard in bin: ");
-    //%08b.%08b.%08b.%08b\n\n", netmaskByte[0], netmaskByte[1], netmaskByte[2], netmaskByte[3]);
-
-    printBinNum(netmaskByte[0]);
-    printf(".");
-    printBinNum(netmaskByte[1]);
-    printf(".");
-    printBinNum(netmaskByte[2]);
-    printf(".");
-    printBinNum(netmaskByte[3]);
+    
+    printf("WildCard: ");
+    printIpDec(netmaskByte, "Wildcard");
+    printf("           ");
+    printIpBin(netmaskByte, "Wildcard");
     printf("\n\n");
 
     int networkByte[4] = {byte[0], byte[1], byte[2], byte[3]};
     calculateNetmaskAndNetwork(networkByte, netmask);
-
-    printf("Network: %d.%d.%d.%d/%d ____ ", networkByte[0], networkByte[1], networkByte[2], networkByte[3], netmask);
-    printf("Network in bin: ");
-    //%08b.%08b.%08b.%08b\n", networkByte[0], networkByte[1], networkByte[2], networkByte[3]);
     
-    printBinNum(networkByte[0]);
-    printf(".");
-    printBinNum(networkByte[1]);
-    printf(".");
-    printBinNum(networkByte[2]);
-    printf(".");
-    printBinNum(networkByte[3]);
+    printf("Network:  ");
+    printIpDec(networkByte, "Network");
+    printf("           ");
+    printIpBin(networkByte, "Network");
     printf("\n");
 
     int brodcastByte[4] = {byte[0], byte[1], byte[2], byte[3]};
     calculateBrodcast(brodcastByte, netmask);
-
-    printf("Brodcast: %d.%d.%d.%d ____ ", brodcastByte[0], brodcastByte[1], brodcastByte[2], brodcastByte[3]);
-    printf("Brodcast in bin: ");
-    //%08b.%08b.%08b.%08b\n", brodcastByte[0], brodcastByte[1], brodcastByte[2], brodcastByte[3]);
     
-    printBinNum(brodcastByte[0]);
-    printf(".");
-    printBinNum(brodcastByte[1]);
-    printf(".");
-    printBinNum(brodcastByte[2]);
-    printf(".");
-    printBinNum(brodcastByte[3]);
+    printf("Brodcast: ");
+    printIpDec(brodcastByte, "Brodcast");
+    printf("           ");
+    printIpBin(brodcastByte, "Brodcast");
     printf("\n");
 
     calculateHostMin(networkByte, netmask);
 
-    printf("HostMin: %d.%d.%d.%d ____ ", networkByte[0], networkByte[1], networkByte[2], networkByte[3]);
-    printf("HostMin in bin: "); 
-    //%08b.%08b.%08b.%08b\n", networkByte[0], networkByte[1], networkByte[2], networkByte[3]);
-
-    printBinNum(networkByte[0]);
-    printf(".");
-    printBinNum(networkByte[1]);
-    printf(".");
-    printBinNum(networkByte[2]);
-    printf(".");
-    printBinNum(networkByte[3]);
+    printf("HostMin:  ");
+    printIpDec(networkByte, "HostMin");
+    printf("           ");
+    printIpBin(networkByte, "HostMin");
     printf("\n");
 
     calculateHostMax(brodcastByte, netmask);
-
-    printf("HostMax: %d.%d.%d.%d ____ ", brodcastByte[0], brodcastByte[1], brodcastByte[2], brodcastByte[3]);
-    printf("HostMax in bin: "); 
-    //%08b.%08b.%08b.%08b\n\n", brodcastByte[0], brodcastByte[1], brodcastByte[2], brodcastByte[3]);
-
-    printBinNum(brodcastByte[0]);
-    printf(".");
-    printBinNum(brodcastByte[1]);
-    printf(".");
-    printBinNum(brodcastByte[2]);
-    printf(".");
-    printBinNum(brodcastByte[3]);
+    
+    printf("HostMax:  ");
+    printIpDec(brodcastByte, "HostMax");
+    printf("           ");
+    printIpBin(brodcastByte, "HostMax");
     printf("\n\n");
 
-    int host = calculateHost(netmask);
-
-    printf("Hosts/Net: %d\n", host);
+    printf("Hosts/Net: %d\n", calculateHost(netmask));
 
     return 0;
 }
